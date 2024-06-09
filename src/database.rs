eb35e4b8d3520614335176d1b590d5f7136f9e80
husky <husky.robot.dog@gmail.com>
2024-06-09 22:40:54 +0800

    coding

--- src/database.rs
+++ src/database.rs
@@ -1,8 +1,11 @@
 use anyhow::Result;
 use once_cell::sync::Lazy;
 use serde::{Deserialize, Serialize};
-use sqlx::{any::AnyPoolOptions, Any, Pool};
-use std::collections::HashMap;
+use sqlx::{
+    any::{AnyConnectOptions, AnyPoolOptions},
+    Any, ConnectOptions, Pool,
+};
+use std::{collections::HashMap, str::FromStr, time::Duration};
 use tokio::sync::RwLock;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -16,6 +19,8 @@ pub struct Config {
     pub url: String,
     pub min_conn: u32,
     pub max_conn: u32,
+    pub show_log: bool,
+    pub slow_query: u64,
 }
 
 static INS: Lazy<RwLock<HashMap<String, Pool<Any>>>> = Lazy::new(|| RwLock::new(HashMap::new()));
@@ -42,10 +47,22 @@ pub async fn init(conf: &Config, key: Option<&str>) -> Result<()> {
         },
     );
 
+    let mut opts = AnyConnectOptions::from_str(&dns)?;
+
+    if conf.show_log {
+        opts = opts.log_statements(log::LevelFilter::Info);
+        opts = opts.log_slow_statements(
+            log::LevelFilter::Warn,
+            Duration::from_millis(conf.slow_query),
+        );
+    } else {
+        opts = opts.disable_statement_logging();
+    }
+
     let pool = AnyPoolOptions::new()
         .min_connections(conf.min_conn)
         .max_connections(conf.max_conn)
-        .connect(&dns)
+        .connect_with(opts)
         .await?;
 
     INS.write()
