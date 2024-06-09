36738301bccddb666879f2bf05a93100226b78c8
husky <husky.robot.dog@gmail.com>
2024-06-09 18:53:54 +0800

    init

---
+++ src/config.rs
@@ -0,0 +1,25 @@
+use anyhow::Result;
+
+pub fn init<T>(name: Option<&str>, defaults: Option<Vec<(&str, config::Value)>>) -> Result<T>
+where
+    T: Sized + for<'a> serde::Deserialize<'a>,
+{
+    let mut settings = config::Config::builder()
+        .add_source(config::File::with_name(name.unwrap_or("config")).required(false))
+        .add_source(
+            config::Environment::with_prefix("APP")
+                .try_parsing(true)
+                .separator("_")
+                .list_separator(" "),
+        );
+
+    if let Some(defaults) = defaults {
+        for (key, value) in defaults {
+            settings = settings.set_default(key, value)?;
+        }
+    }
+
+    let settings = settings.build()?;
+
+    Ok(settings.try_deserialize()?)
+}
---
+++ src/database.rs
@@ -0,0 +1,46 @@
+use anyhow::Result;
+use once_cell::sync::Lazy;
+use serde::{Deserialize, Serialize};
+use sqlx::{any::AnyPoolOptions, Any, Pool};
+use std::collections::HashMap;
+use tokio::sync::RwLock;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Config {
+    pub drive: String,
+    pub hostname: String,
+    pub port: u16,
+    pub username: String,
+    pub password: String,
+    pub database: String,
+    pub url: String,
+    pub min_conn: u32,
+    pub max_conn: u32,
+}
+
+static INS: Lazy<RwLock<HashMap<String, Pool<Any>>>> = Lazy::new(|| RwLock::new(HashMap::new()));
+
+pub async fn ins(key: Option<&str>) -> Pool<Any> {
+    INS.read().await.get(key.unwrap_or("")).cloned().unwrap()
+}
+
+pub async fn init(conf: &Config, key: Option<&str>) -> Result<()> {
+    sqlx::any::install_default_drivers();
+
+    let dns = format!(
+        "{}://{}:{}@{}:{}/{}",
+        conf.drive, conf.username, conf.password, conf.hostname, conf.port, conf.database
+    );
+
+    let pool = AnyPoolOptions::new()
+        .min_connections(conf.min_conn)
+        .max_connections(conf.max_conn)
+        .connect(&dns)
+        .await?;
+
+    INS.write()
+        .await
+        .insert(key.unwrap_or("").to_string(), pool);
+
+    Ok(())
+}
