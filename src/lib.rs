5bdbf8a086b3eaa63ea5730c72f795cc4edcb69d
husky <husky.robot.dog@gmail.com>
2024-06-09 18:55:27 +0800

    coding

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
 pub mod config;
 pub mod database;
 pub mod logger;
+
+#[cfg(feature = "database")]
+pub use sqlx;
+
+pub use anyhow;
+pub use chrono;
+pub use once_cell;
+pub use rust_decimal;
+pub use serde;
+pub use serde_json;
+pub use time;
+pub use tokio;
+pub use tracing;
