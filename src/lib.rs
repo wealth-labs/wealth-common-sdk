d780af3762d13f2c6b1150201d59f29aa65078e6
husky <husky.robot.dog@gmail.com>
2024-06-16 21:51:43 +0800

    coding

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub mod app;
+pub mod code;
 pub mod config;
 pub mod logger;
 
--- src/web.rs
+++ src/web.rs
@@ -1,5 +1,13 @@
+use std::fmt::Display;
+
 use anyhow::Result;
+use axum::{
+	response::{IntoResponse, Response},
+	Json,
+};
+use reqwest::StatusCode;
 use serde::{Deserialize, Serialize};
+use serde_json::{json, Value};
 use tokio::sync::mpsc::{channel, Receiver};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -38,4 +46,51 @@ async fn run(
 		})
 		.await?;
 	Ok(())
+}
+
+#[derive(Debug)]
+pub struct JsonResult {
+	pub code: u64,
+	pub msg: Option<String>,
+	pub data: Value,
+}
+
+impl JsonResult {
+	pub fn new(code: u64, msg: Option<&str>, data: Value) -> Self {
+		Self { code, msg: msg.map(|msg| msg.to_owned()), data }
+	}
+}
+
+impl Display for JsonResult {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		write!(f, "Code({}), Msg({:?}), data({:?})", self.code, self.msg, self.data)
+	}
+}
+
+impl IntoResponse for JsonResult {
+	fn into_response(self) -> Response {
+		(
+			StatusCode::OK,
+			Json(json!({
+				"code":self.code,
+				"msg":self.msg,
+				"data":self.data,
+			})),
+		)
+			.into_response()
+	}
+}
+
+impl<E> From<E> for JsonResult
+where
+	E: Into<anyhow::Error>,
+{
+	fn from(value: E) -> Self {
+		let err: anyhow::Error = value.into();
+		if let Ok(result) = err.downcast::<JsonResult>() {
+			result
+		} else {
+			Self::new(1, Some("server error"), Value::Null)
+		}
+	}
 }
