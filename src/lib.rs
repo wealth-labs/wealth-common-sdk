0e123297ceaf55961b0e42bbd037a691ca34ac77
husky <husky.robot.dog@gmail.com>
2024-06-16 17:29:15 +0800

    coding

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,20 @@
+pub mod config;
+pub mod logger;
+
+#[cfg(feature = "debug")]
+pub use nu_ansi_term;
+
 #[cfg(feature = "database")]
 pub mod database;
 #[cfg(feature = "database")]
 pub use sea_orm;
 
-pub mod config;
-pub mod logger;
+#[cfg(feature = "web")]
+pub use axum;
 
 pub use anyhow;
 pub use async_trait;
 pub use chrono;
-pub use nu_ansi_term;
 pub use once_cell;
 pub use reqwest;
 pub use rust_decimal;
