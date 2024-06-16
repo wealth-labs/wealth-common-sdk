43ec2ba6bfa320a002e54c1fd8c8a93148740479
husky <husky.robot.dog@gmail.com>
2024-06-16 19:23:21 +0800

    coding

--- src/app.rs
+++ src/app.rs
@@ -1,14 +1,19 @@
-use std::sync::Arc;
-
 use anyhow::Result;
 use once_cell::sync::OnceCell;
-use tokio::sync::{oneshot::Receiver, Mutex};
+use std::sync::Arc;
+use tokio::sync::{
+	oneshot::{channel, Receiver, Sender},
+	Mutex,
+};
 
 static INS: OnceCell<App> = OnceCell::new();
 
 #[derive(Debug, Clone)]
-struct App {
-	signals: Arc<Mutex<Vec<(String, Receiver<String>)>>>,
+pub struct App {
+	app_waiting: Arc<Mutex<Vec<(String, Receiver<Result<()>>)>>>,
+	app_stop_notice: Arc<Mutex<Vec<Sender<()>>>>,
+	stop_send: Arc<tokio::sync::mpsc::Sender<()>>,
+	stop_recv: Arc<Mutex<tokio::sync::mpsc::Receiver<()>>>,
 }
 
 pub fn init() -> Result<()> {
@@ -22,25 +27,75 @@ pub fn ins<'a>() -> &'a App {
 
 impl App {
 	fn new() -> Self {
-		Self { signals: Arc::new(Mutex::new(vec![])) }
+		let (stop_send, stop_recv) = tokio::sync::mpsc::channel::<()>(1);
+		let stop_send = Arc::new(stop_send);
+
+		#[cfg(not(unix))]
+		{
+			let stop_send = stop_send.clone();
+			tokio::spawn(async move {
+				tokio::signal::ctrl_c().await.ok();
+				stop_send.send(()).await.ok();
+			});
+		}
+
+		#[cfg(unix)]
+		{
+			let stop_send = stop_send.clone();
+			tokio::spawn(async move {
+				let mut sigterm =
+					tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate())
+						.unwrap();
+				sigterm.recv().await;
+				stop_send.send(()).await.ok();
+			});
+		}
+
+		let app = Self {
+			app_waiting: Arc::new(Mutex::new(vec![])),
+			app_stop_notice: Arc::new(Mutex::new(vec![])),
+			stop_send,
+			stop_recv: Arc::new(Mutex::new(stop_recv)),
+		};
+		app
 	}
-	pub async fn add(&self, name: &str, signal: Receiver<String>) {
-		let mut signals = self.signals.lock().await;
-		signals.push((name.to_owned(), signal));
+
+	pub async fn create_app_stop_notice(&self) -> Receiver<()> {
+		let (tx, rx) = channel::<()>();
+		self.app_stop_notice.lock().await.push(tx);
+		rx
+	}
+
+	pub async fn add_app_waiting(&self, name: &str, signal: Receiver<Result<()>>) {
+		let mut app_waiting = self.app_waiting.lock().await;
+		app_waiting.push((name.to_owned(), signal));
+	}
+
+	pub async fn stop(&self) {
+		self.stop_send.send(()).await.ok();
 	}
 
 	pub async fn waiting(&self) -> Result<()> {
-		tokio::signal::ctrl_c().await?;
-		let mut signals = self.signals.lock().await;
-		for (name, signal) in signals.iter_mut() {
-			println!("application stopping({}) .....", name);
-			let msg = match signal.await {
-				Ok(msg) => msg,
+		tracing::info!("application running .....");
+		self.stop_recv.lock().await.recv().await;
+		tracing::info!("application stopping .....");
+		let mut stop_notices = self.app_stop_notice.lock().await;
+		for stop_notice in stop_notices.iter_mut() {
+			stop_notice.closed().await;
+		}
+		let mut stop_waiting_singals = self.app_waiting.lock().await;
+		for (name, stop_waiting_singal) in stop_waiting_singals.iter_mut() {
+			tracing::info!("application stopping({}) .....", name);
+			let msg = match stop_waiting_singal.await {
+				Ok(msg) => match msg {
+					Ok(_) => "OK".to_owned(),
+					Err(err) => err.to_string(),
+				},
 				Err(err) => err.to_string(),
 			};
-			println!("application stopped({}) : {}", name, msg);
+			tracing::info!("application stopped({}) : {}", name, msg);
 		}
-		println!("application stopped");
+		tracing::info!("application stopped");
 		Ok(())
 	}
 }
--- src/web.rs
+++ src/web.rs
@@ -1,11 +1,41 @@
+use anyhow::Result;
 use serde::{Deserialize, Serialize};
+use tokio::sync::oneshot::channel;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Config {
+	pub name: String,
 	pub listen: String,
 	pub show_log: bool,
 }
 
-pub async fn init() -> Result<()> {
+pub async fn init(
+	config: &Config,
+	register_router: fn(app: axum::Router) -> axum::Router,
+) -> Result<()> {
+	let app_stop_notice = crate::app::ins().create_app_stop_notice().await;
+	let (app_waiting_send, app_waiting_recv) = channel::<Result<()>>();
+	crate::app::ins().add_app_waiting(&config.name, app_waiting_recv).await;
+	let listen = config.listen.to_owned();
+	tokio::spawn(async move {
+		let result = run(listen, register_router, app_stop_notice).await;
+		app_waiting_send.send(result).ok();
+	});
+	Ok(())
+}
+
+async fn run(
+	listen: String,
+	register_router: fn(app: axum::Router) -> axum::Router,
+	app_stop_notice: tokio::sync::oneshot::Receiver<()>,
+) -> Result<()> {
+	let app = axum::Router::new();
+	let app = register_router(app);
+	let listener = tokio::net::TcpListener::bind(listen).await?;
+	axum::serve(listener, app)
+		.with_graceful_shutdown(async move {
+			app_stop_notice.await.ok();
+		})
+		.await?;
 	Ok(())
 }
