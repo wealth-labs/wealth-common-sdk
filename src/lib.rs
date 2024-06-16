7da3a955381b81105f49b53160a82305f659e9e3
husky <husky.robot.dog@gmail.com>
2024-06-16 15:21:00 +0800

    coding

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,16 @@
-pub mod config;
-pub mod logger;
-
 #[cfg(feature = "database")]
 pub mod database;
 #[cfg(feature = "database")]
 pub use sea_orm;
 
+pub mod config;
+pub mod logger;
+
 pub use anyhow;
 pub use async_trait;
 pub use chrono;
 pub use once_cell;
+pub use reqwest;
 pub use rust_decimal;
 pub use serde;
 pub use serde_json;
