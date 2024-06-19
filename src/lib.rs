0a244eba628476b1ea21e4f41fb7e44012e1e6e4
husky <husky.robot.dog@gmail.com>
2024-06-19 20:05:45 +0800

    coding

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![allow(unused)]
+
 pub mod app;
 pub mod config;
 pub mod logger;
@@ -5,20 +7,23 @@ pub mod logger;
 #[cfg(feature = "database")]
 pub mod database;
 #[cfg(feature = "database")]
-pub use sea_orm;
+pub use sea_orm::{self, sea_query::OnConflict as _, EntityTrait as _};
 
 #[cfg(feature = "web")]
 pub mod web;
 #[cfg(feature = "web")]
-pub use axum;
+pub use axum::{self, response::IntoResponse as _};
 
-pub use anyhow;
-pub use async_trait;
-pub use chrono;
-pub use once_cell;
+pub use anyhow::{self as _anyhow, anyhow, bail, ensure, Result};
+pub use async_trait::async_trait;
+pub use chrono::{self, DateTime, Duration};
+pub use once_cell::{self, sync::OnceCell};
 pub use reqwest;
-pub use rust_decimal;
-pub use serde_json;
+pub use rust_decimal::{self, Decimal};
+pub use serde::{self, Deserialize, Serialize};
+pub use serde_json::{self, Map as JsonMap, Value as Json};
+pub use serde_with::{self, serde_as};
+pub use std::{result::Result as StdResult, str::FromStr, time::Duration as StdDuration};
 pub use time;
-pub use tokio;
-pub use tracing;
+pub use tokio::{self, spawn, sync::Mutex, time::sleep};
+pub use tracing::{self, debug, error, info, instrument, warn};
